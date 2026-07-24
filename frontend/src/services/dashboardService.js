0808620b0d6c7e27ce6a4296185fae1c111ec29f
Khushi Arora <arorakhushi1603@gmail.com>
2026-07-24 19:58:24 +0530

    Revert dashboard service

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,64 +1,10 @@
 import API from "./api";
-import {
-  FaFileAlt,
-  FaCalendarCheck,
-  FaUserCheck,
-  FaTimesCircle,
-  FaBriefcase,
-  FaChartLine,
-} from "react-icons/fa";
+
 export const getDashboardStats = async () => {
   const response = await API.get("/dashboard/stats");
-
-  const data = response.data;
-
-  return [
-    {
-      title: "Total Applications",
-      count: data.total,
-      status: "Applications",
-      route: "/my-applications",
-      icon: <FaFileAlt />,
-    },
-    {
-      title: "Applied",
-      count: data.applied,
-      status: "Applied Jobs",
-      route: "/my-applications",
-      icon: <FaBriefcase />,
-    },
-    {
-      title: "Interview",
-      count: data.interview,
-      status: "Upcoming Interviews",
-      route: "/interviews",
-      icon: <FaCalendarCheck />,
-    },
-    {
-      title: "Hired",
-      count: data.hired,
-      status: "Selected",
-      route: "/my-applications",
-      icon: <FaUserCheck />,
-    },
-    {
-      title: "Rejected",
-      count: data.rejected,
-      status: "Rejected",
-      route: "/my-applications",
-      icon: <FaTimesCircle />,
-    },
-    {
-      title: "Screened",
-      count: data.screened,
-      status: "Shortlisted",
-      route: "/my-applications",
-      icon: <FaChartLine />,
-    },
-  ];
+  return response.data;
 };
 
-// Temporary dummy data
 export const getRecentActivities = async () => {
   return [
     {
